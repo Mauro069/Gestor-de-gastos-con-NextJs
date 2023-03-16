6febf132151620b5459fd9873e1ef3d9e8ab1398
Mauro Vera <Maurovera069@gmail.com>
2023-03-16 19:21:29 -0300

    Check if it is number

--- src/utils/withPoints.ts
+++ src/utils/withPoints.ts
@@ -1,9 +1,11 @@
-export const withPoints = (Number: number | string) => {
-  if (Number) {
+export const withPoints = (Number: number) => {
+  console.log("NUMBER => ", Number);
+
+  if (Number && !isNaN(Number)) {
     var str = Number.toString().split(".");
     str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     return str.join(".");
   }
 
-  return null;
+  return 0;
 };
