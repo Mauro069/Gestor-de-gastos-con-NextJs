d1d9ead87611aa4c4b23438c3aece91494e72d62
Mauro Vera <81174890+Mauro069@users.noreply.github.com>
2023-03-17 16:12:42 -0300

    Fix

--- src/pages/api/expenses/[day].ts
+++ src/pages/api/expenses/[day].ts
@@ -4,16 +4,20 @@ import Expense, { IExpense } from "@/models/Expense";
 import { getAmount } from "@/utils/getExpensesAmount";
 import { getPercentage } from "@/utils/getPercentage";
 import { withAuth } from "@/lib/withAuth";
+import { verify } from "jsonwebtoken";
 import db from "@/utils/db";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
     const { day } = req.query;
+    const { gdi_cookie } = req.cookies;
+    const cookie = verify(gdi_cookie!, process.env.JWT_SECRET!);
 
     try {
       await db.connect();
       const expenses: IExpense[] = await Expense.find({
         date: day,
+        userRef: cookie?.data?._id
       });
 
       const todayExpensesAmount = getAmount(expenses);
@@ -21,6 +25,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       const previousDay = getPreviousDay(day);
       const previousDayExpenses: IExpense[] = await Expense.find({
         date: previousDay,
+        userRef: cookie?.data?._id
       });
       const previousDayExpensesAmount = getAmount(previousDayExpenses);
       const percentage = getPercentage(
