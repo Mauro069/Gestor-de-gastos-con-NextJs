b0e47e7843a585282c461206341ad7224e37756b
Mauro Vera <Maurovera069@gmail.com>
2023-03-17 15:17:05 -0300

    Adjust utils

--- src/utils/getPercentage.ts
+++ src/utils/getPercentage.ts
@@ -1,5 +1,5 @@
 export const getPercentage = (previous: number, current: number) => {
   const dif = current - previous;
   const percentage = dif === current ? current : (dif / previous) * 100;
-  return percentage;
+  return percentage.toFixed(2);
 };
--- src/utils/withPoints.ts
+++ src/utils/withPoints.ts
@@ -1,6 +1,4 @@
 export const withPoints = (Number: number) => {
-  console.log("NUMBER => ", Number);
-
   if (Number && !isNaN(Number)) {
     var str = Number.toString().split(".");
     str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
