05a59297e444a018087ae54e41facb5aa1a52f22
Mauro Vera <Maurovera069@gmail.com>
2023-03-21 17:19:21 -0300

    Adjust utils

--- src/utils/getPercentage.ts
+++ src/utils/getPercentage.ts
@@ -1,8 +1,5 @@
 export const getPercentage = (previous: number, current: number) => {
-  console.log("previous =>", previous)
-  console.log("current =>", current)
-
   const dif = current - previous;
   const percentage = dif === current ? current : (dif / previous) * 100;
-  return percentage.toFixed(2);
+  return percentage;
 };
